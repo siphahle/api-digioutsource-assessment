cb2f44c0016000943b676f46036fcf127ff6e7b6
happy0412 <siphahle.nomnganga@gmail.com>
2022-02-24 18:30:27 +0200

    created the framework added new files

--- src/test/java/com/digioutsource/assessment/ExchangeRateTest.java
+++ src/test/java/com/digioutsource/assessment/ExchangeRateTest.java
@@ -9,6 +9,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
+
 public class ExchangeRateTest {
     private static Logger log = LogManager.getLogger(ExchangeRateTest.class.getName());
     Response response;
@@ -23,6 +24,17 @@ public class ExchangeRateTest {
         BaseAssertion.verifyStatusCode(response, 200);
         System.out.println(TestUtils.getResponseString(response));
     }
+    @Test(priority = 1)
+    public void verifyGbpCurrency() {
+        log.info("Starting Create Issue Test");
+        String URI = EndPoint.getEndPoint("?conversion_rates");
+        response = RESTCalls.GETRequest(URI);
+        String strResponse = TestUtils.getResponseString(response);
+        BaseAssertion.verifyTrue(strResponse.contains("GBP"));
+        System.out.println(TestUtils.getResponseString(response));
+    }
+    public void countTotalNumberOfCurrencies(){
 
+    }
 
 }
